c28c8a8893a8b7ad76293fc8ab73b3534502ea88
amitchellrevature <87381948+amitchellrevature@users.noreply.github.com>
2021-09-13 23:22:55 -0400

    Add files via upload

---
+++ Project2/src/main/java/com/revature/Project2/Project2Connect4AppApplication.java
@@ -0,0 +1,13 @@
+package com.revature.Project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2Connect4AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2Connect4AppApplication.class, args);
+	}
+
+}
---
+++ Project2/src/test/java/com/revature/Project2/Project2Connect4AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.Project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2Connect4AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
