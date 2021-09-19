00ba1053608ba8f8c29de2be30eba3f062e75943
amitchellrevature <87381948+amitchellrevature@users.noreply.github.com>
2021-09-19 14:33:13 -0400

    Deleting old directory

--- connect-4-project/src/main/java/com/revature/repository/BoardArchiveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repository;
-
-import com.revature.entity.BoardArchive;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardArchiveRepository extends JpaRepository<BoardArchive, Long> {
-}
