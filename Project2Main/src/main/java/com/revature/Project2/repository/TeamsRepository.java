c1c7d1a5dffa255cced452f6e92628a1ce7a2b2d
amitchellrevature <87381948+amitchellrevature@users.noreply.github.com>
2021-09-20 05:25:27 -0400

    Deleting old files

--- Project2Main/src/main/java/com/revature/Project2/repository/TeamsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.Project2.repository;
-
-import com.revature.Project2.entity.Teams;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeamsRepository extends JpaRepository<Teams, Integer> {
-}
